83609ee385abb27b1ed66eca013c5578e32c741d
frenby52 <aakytin@mail.ru>
2020-02-12 11:49:39 +0300

    adds ArtistQuestionScreen + GenreQuestionScreen

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import PropTypes from "prop-types";
 import WelcomeScreen from "../welcome-screen/welcome-screen.jsx";
+import {BrowserRouter, Route, Switch} from "react-router-dom";
+import ArtistQuestionScreen from "../artist-question-screen/artist-question-screen.jsx";
+import GenreQuestionScreen from "../genre-question-screen/genre-question-screen.jsx";
+
 
 const welcomeButtonClickHandler = () => {};
 
@@ -8,7 +12,21 @@ const App = (props) => {
   const {errorCount} = props;
 
   return (
-    <WelcomeScreen errorCount={errorCount} onWelcomeButtonClick={welcomeButtonClickHandler}/>
+  //  <WelcomeScreen errorCount={errorCount} onWelcomeButtonClick={welcomeButtonClickHandler}/>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          <WelcomeScreen errorCount={errorCount} onWelcomeButtonClick={welcomeButtonClickHandler}/>
+        </Route>
+        <Route exact path="/dev-artist">
+          <ArtistQuestionScreen />
+        </Route>
+        <Route exact path="/dev-genre">
+          <GenreQuestionScreen />
+        </Route>
+      </Switch>
+    </BrowserRouter>
+
   );
 };
 
